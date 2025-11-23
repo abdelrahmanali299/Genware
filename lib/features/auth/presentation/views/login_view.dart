179f0f23f55c16c31905cbe6239fd3b1032f115e
Moaz911gt <waleedmoaz65@gmail.com>
2025-11-23 21:07:24 +0200

    done login view

--- lib/features/auth/presentation/views/login_view.dart
+++ lib/features/auth/presentation/views/login_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:genware/features/auth/presentation/views/widgets/login_view_body.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(body: const LoginViewBody());
   }
 }
---
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:genware/costants.dart';
+import 'package:genware/features/auth/presentation/views/widgets/custom_button.dart';
+import 'package:genware/features/auth/presentation/views/widgets/custom_text_button.dart';
+import 'package:genware/features/auth/presentation/views/widgets/custom_text_form_field.dart';
+
+class LoginViewBody extends StatelessWidget {
+  const LoginViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 32),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        // mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            'Welcome \nBack!',
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.w700),
+          ),
+          const SizedBox(height: 32),
+          CustomTextFormField(
+            icon: Icons.person,
+            text: 'Username or Email',
+            textcolor: Color(0xff676767),
+            iconcolor: Color(0xff676767),
+          ),
+          const SizedBox(height: 32),
+          CustomTextFormField(
+            icon: Icons.lock,
+            text: 'Password',
+            textcolor: Color(0xff676767),
+            iconcolor: Color(0xff676767),
+            isPassword: true,
+          ),
+
+          Align(
+            alignment: AlignmentGeometry.centerRight,
+            child: CustomTextButton(
+              text: 'Forget Password?',
+              textColor: Colors.red,
+            ),
+          ),
+          SizedBox(height: 60),
+          CustomButton(
+            color: kPrimaryColor,
+            text: 'Login',
+            textColor: Colors.white,
+          ),
+          SizedBox(height: 40),
+          Align(
+            alignment: AlignmentGeometry.center,
+            child: Text('- OR Continue with -'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('don\'t have An Account?'),
+              CustomTextButton(text: 'Sign Up', textColor: Colors.red,onPressed: () {
+                
+              },),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
